836bf9f96705c24d576374617529114dd9bc29c7
Ryan Geary <rtgnj42@gmail.com>
2019-03-21 11:09:13 -0400

    Reformat mux

--- mux.vhdl
+++ mux.vhdl
@@ -4,17 +4,16 @@ use ieee.numeric_std.all;
 
 entity mux is
 generic (
-  N : integer:= 2; 
+  N : integer:= 2;
   INLENGTH : integer:= 8); --Number of bits in select
-port( 
+port(
     I:  in std_logic_vector (INLENGTH*2**N-1 downto 0); -- for loading
-    sel: in std_logic_vector(N-1 downto 0); -- Choose input 
-    O:  out std_logic_vector(INLENGTH-1 downto 0) -- output the current register content
-      );
+    sel: in std_logic_vector(N-1 downto 0); -- Choose input
+    O:  out std_logic_vector(INLENGTH-1 downto 0)); -- output the current register content
 end mux;
 
 architecture behav of mux is
 begin
-  O <= I((to_integer(unsigned(sel))+1)*INLENGTH -1 downto to_integer(unsigned(sel))*INLENGTH);  
+  O <= I((to_integer(unsigned(sel))+1)*INLENGTH -1 downto to_integer(unsigned(sel))*INLENGTH);
 end behav;
 
