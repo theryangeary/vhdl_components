26f86c5fe7263e1b2622fe8956c7b5e88f5c93d0
Ryan Geary <rtgnj42@gmail.com>
2019-03-27 14:40:18 -0400

    Complete shift_reg_8

--- shift_reg_8.vhdl
+++ shift_reg_8.vhdl
@@ -1,7 +1,7 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
-entity shift_reg is
+entity shift_reg_8 is
 generic(
     N: integer:= 7
     );
@@ -10,12 +10,12 @@ port(
     I_SHIFT_IN: in std_logic; -- shifted in bit for both left and right
     sel:        in std_logic_vector(1 downto 0); -- 00:hold; 01: shift left; 10: shift right; 11: load
     clock:    in std_logic; -- positive level triggering in problem 3
-    enable:    in std_logic; -- 0: don't do anything; 1: shift_reg is enabled
+    enable:    in std_logic; -- 0: don't do anything; 1: shift_reg_8 is enabled
     O:  out std_logic_vector(N downto 0) -- output the current register content
     );
-end shift_reg;
+end shift_reg_8;
 
-architecture behav of shift_reg is
+architecture behav of shift_reg_8 is
 signal input1, input2, D1, D2, Q1, Q2, left1, left2, right1, right2: std_logic_vector(3 downto 0);
 begin
 
@@ -56,8 +56,8 @@ mux2 : entity work.mux
     wait until (rising_edge(clock));
     if (enable = '1') then
       if (sel = "11") then
-        Q1 <= input1;
-        Q2 <= input2;
+        Q1 <= I(3 downto 0);
+        Q2 <= I(7 downto 4);
       else
         Q1 <= D1;
         Q2 <= D2;
