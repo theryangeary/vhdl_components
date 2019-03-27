d4d2aaeaeed0f1d5f172171acae8f3bb49ae1a4a
Ryan Geary <rtgnj42@gmail.com>
2019-03-26 23:59:34 -0400

    Fix shift_reg and add test bench cases

--- shift_reg.vhdl
+++ shift_reg.vhdl
@@ -5,7 +5,8 @@ entity shift_reg is
 generic(
     N: integer:= 3
     );
-port( I:  in std_logic_vector (N downto 0); -- for loading
+port(
+    I:  in std_logic_vector (N downto 0); -- for loading
     I_SHIFT_IN: in std_logic; -- shifted in bit for both left and right
     sel:        in std_logic_vector(1 downto 0); -- 00:hold; 01: shift left; 10: shift right; 11: load
     clock:    in std_logic; -- positive level triggering in problem 3
@@ -15,25 +16,34 @@ port( I:  in std_logic_vector (N downto 0); -- for loading
 end shift_reg;
 
 architecture behav of shift_reg is
-signal reg, left, right: std_logic_vector(N downto 0);
+signal input, D, Q, left, right: std_logic_vector(N downto 0);
 begin
-  left <= reg(2 downto 0) & I_SHIFT_IN;
-  right <= I_SHIFT_IN & reg(3 downto 1);
+
+  left <= Q(2 downto 0) & I_SHIFT_IN;
+  right <= I_SHIFT_IN & Q(3 downto 1);
+  O <= Q;
+  input <= I;
+
   mux1 : entity work.mux
     generic map(
         INLENGTH => 4)
     port map(
-        A => reg,
+        A => Q,
         B => left,
         C => right,
-        D => I,
+        D => input,
         sel => sel,
-        O => reg);
-  reg_process: process(clock) is
+        O => D
+      );
+
+  reg_process: process is
   begin
-    if (rising_edge(clock)) then
-      if (enable = '1') then
-        O <= reg;
+    wait until (rising_edge(clock));
+    if (enable = '1') then
+      if (sel = "11") then
+        Q <= I;
+      else
+        Q <= D;
       end if;
     end if;
   end process;
