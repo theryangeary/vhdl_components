58c2d894cf67348ac0ccc79e59c78b28db916ee4
Ryan Geary <rtgnj42@gmail.com>
2019-03-29 11:48:36 -0400

    Fix add sub

--- add_sub.vhdl
+++ add_sub.vhdl
@@ -1,5 +1,6 @@
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
 entity add_sub is
 generic(
@@ -34,21 +35,22 @@ begin
       );
   end generate adder_gen;
 
-  over_flow <= carry(N);
-  carry(0) <= '0';
-  process(B, sel)
-    begin
-    if sel = '0' then
-      mux <= B;
-    else
-      mux <= not B;
-    end if;
-  end process;
+  carry(0) <= sel;
+  mux(3) <= B(3) xor sel;
+  mux(2) <= B(2) xor sel;
+  mux(1) <= B(1) xor sel;
+  mux(0) <= B(0) xor sel;
   process is
   begin
     wait until (rising_edge(clock));
       O <= output;
-      over_flow <= carry(N) xor carry(N-1);
+      if (sel = '0') then
+        over_flow <= carry(N) xor carry(N-1);
+        under_flow <= '0';
+      else
+        over_flow <= '0';
+        under_flow <= carry(N) xor carry(N-1);
+      end if;
   end process;
 end behav;
 
